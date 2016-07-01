ebed5fcb1ddc4c08d3a8221107dc9fc12eabdff9
Nick Peihl <nickp@sanjuanco.com>
2016-07-01 16:21:22 -0700

    Add onload to map container

--- elements/maps.js
+++ elements/maps.js
@@ -10,7 +10,7 @@ ${maps.map(function (mapOpt) {
   var el = choo.view`
     <div class="dib ma2 ba bw1 mw7 w-80 w-40-ns bg-near-white tc tl-ns">
     <h2 class="ma2 f4">${mapOpt.id}</h2>
-    ${createMap(mapOpt)}
+    ${createMapElement(mapOpt)}
   </div>
   `
   return el
@@ -19,20 +19,22 @@ ${maps.map(function (mapOpt) {
 `
 }
 
-function createMap (mapOpt) {
+function createMapElement (mapOpt) {
   var mapOptions = mapOpt || {}
   var mapId = mapOptions.id || 'map'
   var el = choo.view`
-<div class="h5 ma2" id="${mapId}"></div>
+  <div class="h5 ma2" id="${mapId}" onload=${function (el) {
+    createMap(el, mapOptions)
+  }}></div>
 `
-  var map = L.map(el, mapOptions)
-  var baseLayer = mapOpt.baseMap || L.esri.tiledMapLayer({
+  return el
+}
+
+function createMap (mapId, mapOptions) {
+  var map = L.map(mapId, mapOptions)
+  var baseLayer = mapOptions.baseMap || L.esri.tiledMapLayer({
     url: 'https://sjcgis.org/arcgis/rest/services/Basemaps/Aerials_2013_WM/MapServer',
     maxZoom: 19
   })
   baseLayer.addTo(map)
-  window.addEventListener('load', function (e) {
-    map.invalidateSize()
-  })
-  return el
 }
