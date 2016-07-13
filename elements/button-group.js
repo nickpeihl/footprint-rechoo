90d77dc9ed86020aeb6e9a24cb2f38eef7304a86
Nick Peihl <nickp@sanjuanco.com>
2016-07-13 10:15:55 -0700

    Fix buttons

--- elements/button-group.js
+++ elements/button-group.js
@@ -5,7 +5,7 @@ module.exports = (state, prev, send) => {
   const el = buttonGroup.map(function (button) {
     return html`
 <div class="fl mw7 w-50 w-25-ns">
-<button id=${button.id} onclick=${onClick} class="db mw4 w-80 ma2" disabled=${button.disabled}>${button.text}</button>
+<button id=${button.id} onclick=${onClick} class="db mw4 w-80 ma2" disabled=${state.buttonGroup.disabled}>${button.text}</button>
 </div>
 `
   })
--- models/buttons.js
+++ models/buttons.js
@@ -4,33 +4,24 @@ module.exports = {
     buttons: [{
       id: 'button1',
       text: 'Button 1',
-      color: 'btn--black',
-      disabled: false
+      color: 'btn--black'
     }, {
       id: 'button2',
       text: 'Button 2',
-      color: 'btn--black',
-      disabled: false
+      color: 'btn--black'
     }, {
       id: 'button3',
       text: 'Button 3',
-      color: 'btn--gray',
-      disabled: false
+      color: 'btn--gray'
     }, {
       id: 'button4',
       text: 'Button 4',
-      color: 'btn--gray',
-      disabled: false
-    }]
+      color: 'btn--gray'
+    }],
+    disabled: false
   },
   reducers: {
-    toggleClickable: function (action, state) {
-      const newButtons = state.buttons.map(function (button) {
-        button.disabled = !button.disabled
-        return button
-      })
-      return { buttonGroup: state.buttons.concat(newButtons) }
-    }
+    toggleClickable: (action, state) => ({disabled: !state.disabled})
   },
   effects: {}
 }
